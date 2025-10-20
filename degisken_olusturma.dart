091fbba85b95f858d588fbb1a28dad43521fe812
Süleyman Büdün <154681896+suleymanbdn@users.noreply.github.com>
2025-10-21 02:58:17 +0300

    Add variable declarations and print statements

---
+++ degisken_olusturma.dart
@@ -0,0 +1,37 @@
+void main (){
+  var ogrenciAdi = "süleyman";
+  var ogrenciYas = 21;
+  var ogrenciBoy = 1.78;
+  var ogrenciBasHarf = "s";
+  var ogrenciDevamEdiyorMu = true;
+
+  print(ogrenciAdi);
+  print(ogrenciYas);
+  print(ogrenciBoy);
+  print(ogrenciBasHarf);
+  print(ogrenciDevamEdiyorMu);
+
+
+  int urun_id = 3416;
+  String urun_adi = "kol saati";
+  int urun_adet = 100;
+  double urun_fiyat = 999.99;
+  String urun_tedarikci = "rolex";
+
+  print("ürün id : $urun_id");
+  print("ürün adı : $urun_adi");
+  print("ürün adedi : $urun_adet");
+  print("ürün fiyat : $urun_fiyat");
+  print("ürün tedarikçisi : $urun_tedarikci");
+
+
+  int a = 100;
+  int b = 200;
+  print("a ile b nin toplamı : ${a+b}");
+
+
+
+
+
+
+}
