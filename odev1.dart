4c30154f6d54b8b698cfbbca64a06b557dee5104
Süleyman Büdün <154681896+suleymanbdn@users.noreply.github.com>
2025-10-21 21:57:41 +0300

    Create odev1.dart

---
+++ odev1.dart
@@ -0,0 +1,146 @@
+ import 'package:flutter/material.dart';
+
+void main(){
+  String ilce = "kadıköy";
+  print("ilce $ilce");
+
+  String kita = "avrupa";
+  print("kıta $kita");
+
+  int faksKod = 47;
+  print(faksKod);
+
+  int postaKodu = 34;
+  print(postaKodu);
+
+  String instaAdresi = "suleymanbudun@...";
+  print(instaAdresi);
+
+  String calistigimBolum = "software";
+  print(calistigimBolum);
+
+  int urunMiktari = 2;
+  print(urunMiktari);
+
+  String musteriSoyad = "budun";
+  print(musteriSoyad);
+
+  double odemeMiktari = 145.99 ;
+  print(odemeMiktari);
+
+  String dogumTarihi = "09.06.2004";
+  print(dogumTarihi);
+
+  int borc = 12000;
+  print("borç : $borc ₺");
+
+  String medeniHal = "bekar";
+  print("medeni hali : $medeniHal");
+
+  String videoYorumlari = "yorum";
+  print(videoYorumlari);
+
+  String odemeSaatleri = "09.10";
+  print(odemeSaatleri);
+
+  double eftMiktari = 200.5;
+  print(eftMiktari);
+
+  int satilanMiktar = 200;
+  print(satilanMiktar);
+
+  String telefonModeli = "iphone";
+  print(telefonModeli);
+
+  String dergiAdi = "bilim çocuk";
+  print(dergiAdi);
+
+  String  basimTarihi = "09.10.2004";
+  print(basimTarihi);
+
+  int zamMiktari = 1000;
+  print("zam miktarı $zamMiktari ₺");
+  
+  int daireSayisi = 10;
+  print(daireSayisi);
+  
+  int enlem = 10;
+  print(enlem);
+
+  int boylam = 20;
+  print(boylam);
+
+  String yemekAdi = "iskender";
+  print(yemekAdi);
+
+  int urunFiyati = 200;
+  print(urunFiyati);
+
+  String sirket = "koç";
+  print(sirket);
+
+  String videoAdi = "video";
+  print(videoAdi);
+
+  String muzikSuresi = "10 dk";
+  print(muzikSuresi);
+
+  int mekanPuani = 5;
+  print(mekanPuani);
+
+  String dosyaAdi = "dosya";
+  print(dosyaAdi);
+
+  String resimFormati = "png";
+  print(resimFormati);
+
+  String renk = "kırmızı";
+  print(renk);
+
+  String renkKodu = "123";
+  print(renkKodu);
+
+  String pcModel = "asus";
+  print(pcModel);
+
+  double ekranBoyutu = 15.5;
+  print(ekranBoyutu);
+
+  String kullanimSuresi = "10 sa";
+  print(kullanimSuresi);
+
+  int basinc = 20;
+  print(basinc);
+
+  String etkinlikGunu = "pzt";
+  print(etkinlikGunu);
+
+  String odemeGunu = "salı";
+  print(odemeGunu);
+
+  String cikisTarihi = "09.10.2025";
+  print(cikisTarihi);
+
+  String mahalleAdi = "mahalle";
+  print(mahalleAdi);
+
+  String otobusHatti = "hat";
+  print(otobusHatti);
+
+  int kullanilanDakika = 20;
+  print(kullanilanDakika);
+
+  int kargoTakipNo = 12;
+  print(kargoTakipNo);
+
+  String kuponSuresi = "20 gün";
+  print(kuponSuresi);
+
+  String kuponKodu = "20abc";
+  print(kuponKodu);
+  
+  String faturaTarihi = "20 kasım 2024";
+  print(faturaTarihi);
+
+
+}
