9dd7b32f7172e0055d2ed08fe18d94408edb2edc
Süleyman Büdün <154681896+suleymanbdn@users.noreply.github.com>
2025-10-21 03:43:48 +0300

    Add sabitler.dart with variable examples
    Demonstrate the use of variables, final, and const in Dart.

---
+++ sabitler.dart
@@ -0,0 +1,21 @@
+void main(){
+  //Sabitler
+  //final, const
+  int sayi = 30;
+  print(sayi);
+  sayi = 100;
+  print (sayi);
+  
+  // final: kodlamayı çalıştırdığımızda hafızada oluşur.
+  final int numara = 100;
+  print(numara);
+  //numara = 200; hata
+  
+  
+  /* const: değişkeni tanımladığımızda hafızada oluşur.
+     !!!genellikle görsel nesnelerde kullanırız!!!
+   */
+  const int sonuc = 200;
+  print(sonuc);
+  //sonuc = 300; hata 
+}
