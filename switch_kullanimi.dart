fc3c6154116612dd2614ebc20d1612d961ab752d
Süleyman Büdün <154681896+suleymanbdn@users.noreply.github.com>
2025-10-25 15:12:13 +0300

    Add switch case example for day of the week

---
+++ switch_kullanimi.dart
@@ -0,0 +1,23 @@
+void main(){
+  int gun = 3;
+
+  switch(gun){
+    case 1: print("pazartesi");
+    break;
+    case 2: print("salı");
+    break;
+    case 3: print("çarşamba");
+    break;
+    case 4: print("perşembe");
+    break;
+    case 5: print("cuma");
+    break;
+    case 6: print("cumartesi");
+    break;
+    case 7: print("pazar");
+    break;
+    default: print("böyle bir gün yok");
+    break;
+
+  }
+}
